e15c3e73c066a5992dc2076173a59e364f6f06ff
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-10-18 15:20:02 +0330

    ui check

--- src/components/FinanceChart.tsx
+++ src/components/FinanceChart.tsx
@@ -1,60 +1,126 @@
-import Image from 'next/image'
-import React from 'react';
+"use client";
+import Image from "next/image";
+import React, { PureComponent } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const FinanceChart = () => {
+  const data = [
+    {
+      name: "Jan",
+      income: 4000,
+      expense: 2400,
+    },
+    {
+      name: "Feb",
+      income: 3000,
+      expense: 1398,
+    },
+    {
+      name: "Mar",
+      income: 2000,
+      expense: 9800,
+    },
+    {
+      name: "Apr",
+      income: 2780,
+      expense: 3908,
+    },
+    {
+      name: "May",
+      income: 1890,
+      expense: 4800,
+    },
+    {
+      name: "Jun",
+      income: 2390,
+      expense: 3800,
+    },
+    {
+      name: "Jul",
+      income: 3490,
+      expense: 4300,
+    },
 
-// const data = [
-//   {
-//     name: 'Page A',
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: 'Page B',
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: 'Page C',
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: 'Page D',
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: 'Page E',
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: 'Page F',
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: 'Page G',
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
-// ];
+    {
+      name: "Aug",
+      income: 3490,
+      expense: 4300,
+    },
+
+    {
+      name: "Sep",
+      income: 3490,
+      expense: 4300,
+    },
+
+    {
+      name: "Oct",
+      income: 3490,
+      expense: 4300,
+    },
+
+    {
+      name: "Nov",
+      income: 3490,
+      expense: 4300,
+    },
+
+    {
+      name: "Dec",
+      income: 3490,
+      expense: 4300,
+    },
+  ];
   return (
     <div className="bg-white rounded-lg p-4 h-full">
       <div className="flex items-center justify-between">
         <h1 className="font-semibold text-lg">Attendance</h1>
         <Image src="/moreDark.png" alt="more..." width={20} height={20} />
       </div>
-    </div>
-  )
-}
+      <ResponsiveContainer width="100%" height="90%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" stroke="#ddd" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#d1d6db" }}
+            tickLine={false}
+            tickMargin={10}
+          />
+          <YAxis tick={{ fill: "#d1d6db" }} tickLine={false} axisLine={false} />
+          <Tooltip />
+          <Legend
+          
+            align="center"
+            verticalAlign="top"
+            wrapperStyle={{ paddingTop: "10px", paddingBottom: "30px", fontWeight:"bold"  }}
+          />
 
-export default FinanceChart
+          <Line dot={false} type="monotone" dataKey="income" stroke="#c3ebfa" strokeWidth={4} />
+          <Line dot={false} type="monotone" dataKey="expense" stroke="#CFCEFF" strokeWidth={4} />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default FinanceChart;
